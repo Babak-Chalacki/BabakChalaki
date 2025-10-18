733a422d4c6cdc87fdd6e123c7039c2a2eeb6a20
Babak-Chalacki <babak.chalaki82@gmail.com>
2025-10-18 17:16:10 +0330

    fix coffee icon

---
+++ script.js
@@ -0,0 +1,6 @@
+let coffeIcon = document.getElementById('coffee-icon');
+let donateBox = document.getElementById('donate-box');
+
+donateBox.addEventListener('click', () => {
+    console.log('hello')
+})
